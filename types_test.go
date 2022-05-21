5137ad1d85544374896057d95dff0177cf4d046e
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-05-21 08:46:12 -0700

    Add unit tests for the encoding types.

---
+++ types_test.go
@@ -0,0 +1,48 @@
+package chirpstore_test
+
+import (
+	"testing"
+
+	"github.com/creachadair/chirpstore"
+	"github.com/google/go-cmp/cmp"
+)
+
+type roundTripper interface {
+	Encode() []byte
+	UnmarshalBinary([]byte) error
+}
+
+func testRoundTrip(t *testing.T, out, in roundTripper) {
+	t.Helper()
+
+	bits := in.Encode()
+	t.Logf("Encoding: %+q", bits)
+	if err := out.UnmarshalBinary(bits); err != nil {
+		t.Fatalf("UnmarshalBinary: %v", err)
+	}
+	if diff := cmp.Diff(in, out); diff != "" {
+		t.Fatalf("Wrong decoded value (-want, +got):\n%s", diff)
+	}
+}
+
+func TestTypes(t *testing.T) {
+	t.Run("PutRequest", func(t *testing.T) {
+		testRoundTrip(t, new(chirpstore.PutRequest), &chirpstore.PutRequest{
+			Key:     []byte("hey there delilah"),
+			Data:    []byte("what's it like in new york city"),
+			Replace: true,
+		})
+	})
+	t.Run("ListRequest", func(t *testing.T) {
+		testRoundTrip(t, new(chirpstore.ListRequest), &chirpstore.ListRequest{
+			Start: []byte("the coolth of your evening smile"),
+			Count: 122,
+		})
+	})
+	t.Run("ListResponse", func(t *testing.T) {
+		testRoundTrip(t, new(chirpstore.ListResponse), &chirpstore.ListResponse{
+			Next: []byte("toad"),
+			Keys: [][]byte{[]byte("crossroads"), []byte("spoonful"), []byte("train time")},
+		})
+	})
+}
