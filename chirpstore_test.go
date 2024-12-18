b60094f96c9f8e35cd6a54c39bb59dd0cc98a981
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-12-18 12:15:17 -0800

    chirpstore: update to the new blob.Store interface (#1)
    See creachadair/ffs#26.
    
    N.B.: go.mod pins ffs to commit ea9c7142ea51 for now

--- chirpstore_test.go
+++ chirpstore_test.go
@@ -17,8 +17,11 @@ import (
 )
 
 // Interface satisfaction checks.
-var _ blob.KV = chirpstore.KV{}
-var _ blob.CAS = chirpstore.CAS{}
+var (
+	_ blob.KV        = chirpstore.KV{}
+	_ blob.CAS       = chirpstore.KV{}
+	_ blob.SyncKeyer = chirpstore.KV{}
+)
 
 var doDebug = flag.Bool("debug", false, "Enable debug logging")
 
@@ -29,17 +32,16 @@ func logPacket(t *testing.T, tag string) chirp.PacketLogger {
 	}
 }
 
-func newTestService(t *testing.T, bs blob.KV) *chirp.Peer {
+func newTestService(t *testing.T, bs blob.Store) *chirp.Peer {
 	if bs == nil {
-		bs = memstore.New()
+		bs = memstore.New(nil)
 	}
 	svc := chirpstore.NewService(bs, nil)
 
 	loc := peers.NewLocal()
 	svc.Register(loc.A)
 	if *doDebug {
-		loc.A.LogPackets(logPacket(t, "A"))
-		loc.B.LogPackets(logPacket(t, "B"))
+		loc.A.LogPackets(logPacket(t, "srv"))
 	}
 	t.Cleanup(func() {
 		if err := loc.Stop(); err != nil {
@@ -49,32 +51,27 @@ func newTestService(t *testing.T, bs blob.KV) *chirp.Peer {
 	return loc.B
 }
 
-func TestKV(t *testing.T) {
-	t.Run("KV", func(t *testing.T) {
-		peer := newTestService(t, nil)
-		rs := chirpstore.NewKV(peer, nil)
-		storetest.Run(t, rs)
-	})
-	t.Run("CAS", func(t *testing.T) {
-		peer := newTestService(t, nil)
-		rs := chirpstore.NewCAS(peer, nil)
-		storetest.Run(t, rs)
-	})
+func TestStore(t *testing.T) {
+	peer := newTestService(t, nil)
+	rs := chirpstore.NewStore(peer, nil)
+	storetest.Run(t, rs)
 }
 
 func TestCAS(t *testing.T) {
-	mem := blob.NewCAS(memstore.New(), sha1.New)
-	peer := newTestService(t, mem)
+	peer := newTestService(t, memstore.New(func() blob.KV {
+		return blob.NewCAS(memstore.NewKV(), sha1.New)
+	}))
 
 	// echo "abcde" | shasum -a 1
 	const input = "abcde\n"
 	const want = "ec11312386ad561674f724b8cca7cf1796e26d1d"
 
 	ctx := context.Background()
-	rs := chirpstore.NewCAS(peer, nil)
+	rs := chirpstore.NewStore(peer, nil)
+	kv := storetest.SubKeyspace(t, ctx, rs, "").(chirpstore.KV)
 
 	t.Run("CASPut", func(t *testing.T) {
-		key, err := rs.CASPut(ctx, blob.CASPutOptions{Data: []byte(input)})
+		key, err := kv.CASPut(ctx, blob.CASPutOptions{Data: []byte(input)})
 		if err != nil {
 			t.Errorf("PutCAS(%q) failed: %v", input, err)
 		} else if got := fmt.Sprintf("%x", key); got != want {
@@ -83,7 +80,7 @@ func TestCAS(t *testing.T) {
 	})
 
 	t.Run("CASKey", func(t *testing.T) {
-		key, err := rs.CASKey(ctx, blob.CASPutOptions{Data: []byte(input)})
+		key, err := kv.CASKey(ctx, blob.CASPutOptions{Data: []byte(input)})
 		if err != nil {
 			t.Errorf("CASKey(%q) failed: %v", input, err)
 		} else if got := fmt.Sprintf("%x", key); got != want {
@@ -92,7 +89,7 @@ func TestCAS(t *testing.T) {
 	})
 
 	t.Run("Len", func(t *testing.T) {
-		n, err := rs.Len(ctx)
+		n, err := kv.Len(ctx)
 		if err != nil {
 			t.Errorf("Len failed: %v", err)
 		} else if n != 1 {
@@ -102,22 +99,25 @@ func TestCAS(t *testing.T) {
 }
 
 func TestSyncKeyer(t *testing.T) {
-	peer := newTestService(t, memstore.New().Init(map[string]string{
-		"one": "1", "two": "2", "three": "3", "four": "4",
+	peer := newTestService(t, memstore.New(func() blob.KV {
+		return memstore.NewKV().Init(map[string]string{
+			"one": "1", "two": "2", "three": "3", "four": "4",
+		})
 	}))
 
 	ctx := context.Background()
-	rs := chirpstore.NewKV(peer, nil)
+	rs := chirpstore.NewStore(peer, nil)
+	kv := storetest.SubKeyspace(t, ctx, rs, "").(chirpstore.KV)
 
 	t.Run("NoneMissing", func(t *testing.T) {
-		if got, err := rs.SyncKeys(ctx, []string{"one", "three", "two"}); err != nil {
+		if got, err := kv.SyncKeys(ctx, []string{"one", "three", "two"}); err != nil {
 			t.Fatalf("SyncKeys: unexpected error: %v", err)
 		} else if len(got) != 0 {
 			t.Fatalf("SyncKeys: got %q, want empty", got)
 		}
 	})
 	t.Run("SomeMissing", func(t *testing.T) {
-		if got, err := rs.SyncKeys(ctx, []string{"one", "three", "five"}); err != nil {
+		if got, err := kv.SyncKeys(ctx, []string{"one", "three", "five"}); err != nil {
 			t.Fatalf("SyncKeys: unexpected error: %v", err)
 		} else if diff := gocmp.Diff(got, []string{"five"}); diff != "" {
 			t.Fatalf("SyncKeys (-got, +want):\n%s", diff)
--- service.go
+++ service.go
@@ -3,6 +3,8 @@ package chirpstore
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sync"
 
 	"github.com/creachadair/chirp"
 	"github.com/creachadair/ffs/blob"
@@ -10,7 +12,10 @@ import (
 
 // Constants defining the method names for the store service.
 const (
-	mStatus   = "status"
+	// Metadata.
+	mStatus = "status"
+
+	// Keyspace (KV) methods.
 	mGet      = "get"
 	mPut      = "put"
 	mDelete   = "delete"
@@ -19,26 +24,39 @@ const (
 	mCASPut   = "cas-put"
 	mCASKey   = "cas-key"
 	mSyncKeys = "sync-keys"
+
+	// Store methods.
+	mKeyspace = "keyspace"
+	mSubstore = "substore"
 )
 
 type Service struct {
 	pfx string
-	st  blob.SyncKeyer
-	cas blob.CAS // populated iff st implements blob.CAS
+
+	μ      sync.Mutex
+	lastID int
+	subs   map[int]*storeInfo
+	kvs    map[int]blob.KV
 }
 
 // NewService constructs a service that delegates to the given [blob.KV].
-func NewService(st blob.KV, opts *ServiceOptions) *Service {
-	s := &Service{pfx: opts.prefix()}
-	if sk, ok := st.(blob.SyncKeyer); ok {
-		s.st = sk
-	} else {
-		s.st = blob.ListSyncKeyer{KV: st}
-	}
-	if cas, ok := st.(blob.CAS); ok {
-		s.cas = cas
+func NewService(st blob.Store, opts *ServiceOptions) *Service {
+	s := &Service{
+		pfx:  opts.prefix(),
+		subs: map[int]*storeInfo{0: newStoreInfo(st)},
+		kvs:  make(map[int]blob.KV),
 	}
 	return s
+}
+
+type storeInfo struct {
+	store blob.Store
+	subs  map[string]int // name to store ID
+	kvs   map[string]int // name to keyspace ID
+}
+
+func newStoreInfo(st blob.Store) *storeInfo {
+	return &storeInfo{store: st, subs: make(map[string]int), kvs: make(map[string]int)}
 }
 
 // ServiceOptions provides optional settings for constructing a [Service].
@@ -65,47 +83,132 @@ func (s *Service) Register(p *chirp.Peer) {
 	p.Handle(s.method(mList), s.List)
 	p.Handle(s.method(mLen), s.Len)
 	p.Handle(s.method(mSyncKeys), s.SyncKeys)
-	if s.cas != nil {
-		p.Handle(s.method(mCASPut), s.CASPut)
-		p.Handle(s.method(mCASKey), s.CASKey)
+	p.Handle(s.method(mCASPut), s.CASPut)
+	p.Handle(s.method(mCASKey), s.CASKey)
+	p.Handle(s.method(mKeyspace), s.Keyspace)
+	p.Handle(s.method(mSubstore), s.Sub)
+}
+
+// Keyspace implements the eponymous method of the [blob.Store] interface.
+// The client is returned an integer descriptor (ID) that must be presented in
+// subsequent requests to identify which keyspace to affect.
+func (s *Service) Keyspace(ctx context.Context, req *chirp.Request) ([]byte, error) {
+	var kreq KeyspaceRequest
+	if err := kreq.Decode(req.Data); err != nil {
+		return nil, err
 	}
+	s.μ.Lock()
+	defer s.μ.Unlock()
+
+	si := s.subs[kreq.ID]
+	if si == nil {
+		return nil, fmt.Errorf("invalid store ID %d", kreq.ID)
+	}
+	name := string(kreq.Key)
+	kvID, ok := si.kvs[name]
+	if !ok {
+		kv, err := si.store.Keyspace(ctx, name)
+		if err != nil {
+			return nil, fmt.Errorf("create keyspace %q in store %d: %w", name, kreq.ID, err)
+		}
+		s.lastID++
+		kvID = s.lastID
+		s.kvs[kvID] = kv
+		si.kvs[name] = kvID
+	}
+	return KeyspaceResponse{ID: kvID}.Encode(), nil
+}
+
+// Sub implements the eponymous method of the [blob.Store] interface.
+// The client is returned an integer descriptor (ID) that must be presented in
+// subsequent substore and keyspace requests to identify which store to affect.
+func (s *Service) Sub(ctx context.Context, req *chirp.Request) ([]byte, error) {
+	var sreq SubRequest
+	if err := sreq.Decode(req.Data); err != nil {
+		return nil, err
+	}
+	s.μ.Lock()
+	defer s.μ.Unlock()
+
+	si := s.subs[sreq.ID]
+	if si == nil {
+		return nil, fmt.Errorf("invalid store ID %d", sreq.ID)
+	}
+	name := string(sreq.Key)
+	subID, ok := si.subs[name]
+	if !ok {
+		sub, err := si.store.Sub(ctx, name)
+		if err != nil {
+			return nil, fmt.Errorf("create substore %q in store %d: %w", name, sreq.ID, err)
+		}
+		s.lastID++
+		subID = s.lastID
+		s.subs[subID] = newStoreInfo(sub)
+		si.subs[name] = subID
+	}
+	return SubResponse{ID: subID}.Encode(), nil
 }
 
 // Status returns a JSON blob of server metrics.
 func (s *Service) Status(ctx context.Context, req *chirp.Request) ([]byte, error) {
+	// TODO(creachadair): Add some metrics about substore and keyspace usage.
 	mx := chirp.ContextPeer(ctx).Metrics()
 	return []byte(mx.String()), nil
 }
 
-// Get handles the corresponding method of blob.Store.
+// Get handles the corresponding method of [blob.KV].
 func (s *Service) Get(ctx context.Context, req *chirp.Request) ([]byte, error) {
-	data, err := s.st.Get(ctx, string(req.Data))
+	var greq GetRequest
+	if err := greq.Decode(req.Data); err != nil {
+		return nil, err
+	}
+	kv := s.idToKV(greq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(greq.ID)
+	}
+	data, err := kv.Get(ctx, string(greq.Key))
 	return data, filterErr(err)
 }
 
-// Put handles the corresponding method of blob.Store.
+// Put handles the corresponding method of [blob.KV].
 func (s *Service) Put(ctx context.Context, req *chirp.Request) ([]byte, error) {
 	var preq PutRequest
 	if err := preq.Decode(req.Data); err != nil {
 		return nil, err
 	}
-	return nil, filterErr(s.st.Put(ctx, blob.PutOptions{
+	kv := s.idToKV(preq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(preq.ID)
+	}
+	return nil, filterErr(kv.Put(ctx, blob.PutOptions{
 		Key:     string(preq.Key),
 		Data:    preq.Data,
 		Replace: preq.Replace,
 	}))
 }
 
-// Delete handles the corresponding method of blob.Store.
+// Delete handles the corresponding method of [blob.KV].
 func (s *Service) Delete(ctx context.Context, req *chirp.Request) ([]byte, error) {
-	return nil, filterErr(s.st.Delete(ctx, string(req.Data)))
+	var dreq DeleteRequest
+	if err := dreq.Decode(req.Data); err != nil {
+		return nil, err
+	}
+	kv := s.idToKV(dreq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(dreq.ID)
+	}
+	return nil, filterErr(kv.Delete(ctx, string(dreq.Key)))
 }
 
-// List handles the corresponding method of blob.Store.
+// List handles the corresponding method of [blob.KV].
 func (s *Service) List(ctx context.Context, req *chirp.Request) ([]byte, error) {
 	var lreq ListRequest
 	if err := lreq.Decode(req.Data); err != nil {
 		return nil, err
+	}
+	kv := s.idToKV(lreq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(lreq.ID)
 	}
 
 	limit := lreq.Count
@@ -114,7 +217,7 @@ func (s *Service) List(ctx context.Context, req *chirp.Request) ([]byte, error) 
 	}
 
 	var lrsp ListResponse
-	if err := s.st.List(ctx, string(lreq.Start), func(key string) error {
+	if err := kv.List(ctx, string(lreq.Start), func(key string) error {
 		if len(lrsp.Keys) == limit {
 			lrsp.Next = []byte(key)
 			return blob.ErrStopListing
@@ -127,26 +230,39 @@ func (s *Service) List(ctx context.Context, req *chirp.Request) ([]byte, error) 
 	return lrsp.Encode(), nil
 }
 
-// Len handles the corresponding method of blob.Store.
+// Len handles the corresponding method of [blob.KV].
 func (s *Service) Len(ctx context.Context, req *chirp.Request) ([]byte, error) {
-	size, err := s.st.Len(ctx)
+	var lreq LenRequest
+	if err := lreq.Decode(req.Data); err != nil {
+		return nil, err
+	}
+	kv := s.idToKV(lreq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(lreq.ID)
+	}
+	size, err := kv.Len(ctx)
 	if err != nil {
 		return nil, err
 	}
 	return packInt64(size), nil
 }
 
-// CASPut implements content-addressable storage if the service has a CAS.
-// It reports an error if the underlying store is not a blob.CAS.
+// CASPut implements the corresponding method of [blob.CAS].
+// It reports an error if the underlying keyspace does not implement it.
 func (s *Service) CASPut(ctx context.Context, req *chirp.Request) ([]byte, error) {
-	if s.cas == nil {
-		return nil, errors.New("store does not implement content addressing")
-	}
 	var preq CASPutRequest
 	if err := preq.Decode(req.Data); err != nil {
 		return nil, err
 	}
-	key, err := s.cas.CASPut(ctx, blob.CASPutOptions{
+	kv := s.idToKV(preq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(preq.ID)
+	}
+	cas, ok := kv.(blob.CAS)
+	if !ok {
+		return nil, errors.New("KV does not implement content addressing")
+	}
+	key, err := cas.CASPut(ctx, blob.CASPutOptions{
 		Data:   preq.Data,
 		Prefix: string(preq.Prefix),
 		Suffix: string(preq.Suffix),
@@ -154,17 +270,22 @@ func (s *Service) CASPut(ctx context.Context, req *chirp.Request) ([]byte, error
 	return []byte(key), err
 }
 
-// CASKey computes the hash key for the specified data, if the service has a CAS.
-// It reports an error if the underlying store is not a blob.CAS.
+// CASKey implements the corresponding method of [blob.CAS].
+// It reports an error if the underlying keyspace does not implement it.
 func (s *Service) CASKey(ctx context.Context, req *chirp.Request) ([]byte, error) {
-	if s.cas == nil {
-		return nil, errors.New("store does not implement content addressing")
-	}
 	var preq CASPutRequest
 	if err := preq.Decode(req.Data); err != nil {
 		return nil, err
 	}
-	key, err := s.cas.CASKey(ctx, blob.CASPutOptions{
+	kv := s.idToKV(preq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(preq.ID)
+	}
+	cas, ok := kv.(blob.CAS)
+	if !ok {
+		return nil, errors.New("KV does not implement content addressing")
+	}
+	key, err := cas.CASKey(ctx, blob.CASPutOptions{
 		Data:   preq.Data,
 		Prefix: string(preq.Prefix),
 		Suffix: string(preq.Suffix),
@@ -172,16 +293,37 @@ func (s *Service) CASKey(ctx context.Context, req *chirp.Request) ([]byte, error
 	return []byte(key), err
 }
 
-// SyncKeys reports which of the specified keys are not present in the store.
+// SyncKeys implements the required method of [blob.SyncKeyer].
+// If the underlying keyspace does not implement it, a wrapper is provided
+// using [blob.ListSyncKeyer].
 func (s *Service) SyncKeys(ctx context.Context, req *chirp.Request) ([]byte, error) {
 	var sreq SyncRequest
 	if err := sreq.Decode(req.Data); err != nil {
 		return nil, err
 	}
-	missing, err := s.st.SyncKeys(ctx, sreq.getKeys())
+	kv := s.idToKV(sreq.ID)
+	if kv == nil {
+		return invalidKeyspaceID(sreq.ID)
+	}
+	sync, ok := kv.(blob.SyncKeyer)
+	if !ok {
+		sync = blob.ListSyncKeyer{KV: kv}
+	}
+	missing, err := sync.SyncKeys(ctx, getKeys(&sreq.Keys))
 	if err != nil {
 		return nil, err
 	}
-	sreq.setKeys(missing)
-	return sreq.Encode(), nil
+	var srsp SyncResponse
+	setKeys(&srsp.Missing, missing)
+	return srsp.Encode(), nil
+}
+
+func (s *Service) idToKV(id int) blob.KV {
+	s.μ.Lock()
+	defer s.μ.Unlock()
+	return s.kvs[id]
+}
+
+func invalidKeyspaceID(id int) ([]byte, error) {
+	return nil, fmt.Errorf("invalid keyspace ID %d", id)
 }
