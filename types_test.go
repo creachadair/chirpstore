f573d8e18c391bcb376098b81e3d172ae4412aac
M. J. Fromberger <michael.j.fromberger@gmail.com>
2026-06-13 10:56:56 -0700

    types: simplify the test helper with a type constraint

--- types_test.go
+++ types_test.go
@@ -7,58 +7,62 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-type roundTripper interface {
+type roundTripper[T any] interface {
+	*T // the receiver must be a pointer
+
 	Encode() []byte
 	Decode([]byte) error
 }
 
-func testRoundTrip(out, in roundTripper) func(*testing.T) {
+func testRoundTrip[T any, TP roundTripper[T]](in TP) func(*testing.T) {
 	return func(t *testing.T) {
 		t.Helper()
 
 		bits := in.Encode()
-		t.Logf("Encoding: %+q", bits)
+		t.Logf("Encoding of %T: %+q", in, bits)
+
+		var out TP = new(T)
 		if err := out.Decode(bits); err != nil {
 			t.Fatalf("UnmarshalBinary: %v", err)
 		}
-		if diff := cmp.Diff(in, out); diff != "" {
-			t.Fatalf("Wrong decoded value (-want, +got):\n%s", diff)
+		if diff := cmp.Diff(out, in); diff != "" {
+			t.Fatalf("Wrong decoded value (-got, +want):\n%s", diff)
 		}
 	}
 }
 
 func TestTypes(t *testing.T) {
-	t.Run("PutRequest", testRoundTrip(new(chirpstore.PutRequest), &chirpstore.PutRequest{
+	t.Run("PutRequest", testRoundTrip(&chirpstore.PutRequest{
 		ID:      1,
 		Key:     []byte("hey there delilah"),
 		Data:    []byte("what's it like in new york city"),
 		Replace: true,
 	}))
-	t.Run("ListRequest", testRoundTrip(new(chirpstore.ListRequest), &chirpstore.ListRequest{
+	t.Run("ListRequest", testRoundTrip(&chirpstore.ListRequest{
 		ID:    2,
 		Start: []byte("the coolth of your evening smile"),
 		Count: 122,
 	}))
-	t.Run("ListResponse", testRoundTrip(new(chirpstore.ListResponse), &chirpstore.ListResponse{
+	t.Run("ListResponse", testRoundTrip(&chirpstore.ListResponse{
 		Next: []byte("toad"),
 		Keys: keyBytes("crossroads", "spoonful", "train time"),
 	}))
-	t.Run("HasRequest", testRoundTrip(new(chirpstore.HasRequest), &chirpstore.HasRequest{
+	t.Run("HasRequest", testRoundTrip(&chirpstore.HasRequest{
 		ID:   4,
 		Keys: []string{"apple", "", "pear", "plum", "cherry"},
 	}))
-	t.Run("GetRequest", testRoundTrip(new(chirpstore.GetRequest), &chirpstore.GetRequest{
+	t.Run("GetRequest", testRoundTrip(&chirpstore.GetRequest{
 		ID:  5,
 		Key: []byte("fall into sleep, fall into me"),
 	}))
-	t.Run("KeyspaceRequest", testRoundTrip(new(chirpstore.KeyspaceRequest), &chirpstore.KeyspaceRequest{
+	t.Run("KeyspaceRequest", testRoundTrip(&chirpstore.KeyspaceRequest{
 		ID:  6,
 		Key: []byte("la padrona soy yo"),
 	}))
-	t.Run("KeyspaceResponse", testRoundTrip(new(chirpstore.KeyspaceResponse), &chirpstore.KeyspaceResponse{
+	t.Run("KeyspaceResponse", testRoundTrip(&chirpstore.KeyspaceResponse{
 		ID: 7,
 	}))
-	t.Run("LenRequest", testRoundTrip(new(chirpstore.LenRequest), &chirpstore.LenRequest{
+	t.Run("LenRequest", testRoundTrip(&chirpstore.LenRequest{
 		ID: 8,
 	}))
 }
